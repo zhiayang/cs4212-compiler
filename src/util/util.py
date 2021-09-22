659e6fbc56f6425d7c272f953f1a8c36d0f8c59b
zhiayang <gpg@tsumaran.ai>
2021-09-22 15:28:10 +0800

    pretty-print errors

--- src/util/util.py
+++ src/util/util.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 from typing import *
 
 import sys
+import os
 
 def read_entire_file(filename: str) -> str:
 	with open(filename, "rb") as f:
@@ -24,12 +25,36 @@ class Location:
 	def __str__(self) -> str:
 		return f"{self.filename}:{self.line + 1}:{self.column + 1}"
 
-class ParseException(BaseException):
+def colourise(msg: str, colour: str) -> str:
+	if sys.stdout.isatty():
+		return f"\x1b[{colour}{msg}\x1b[0m"
+	else:
+		return msg
+
+
+TAB_WIDTH = 4
+
+class ParseException(Exception):
 	def __init__(self, loc: Location, msg: str) -> None:
 		self.loc: Location = loc
 		self.msg: str = msg
 
-		# actually, exit the program now.
-		print(f"{self.loc}: {msg}")
+	def throw(self) -> NoReturn:
+		print(f"{self.loc}: {colourise('error:', '1;31m')} {colourise(self.msg, '1m')}")
+
+		# print the gutter
+		gutter_width = 4 + len(str(1 + self.loc.line))
+
+		# we have the filename, so just read the file again
+		offending_code: str = open(self.loc.filename, "rb").read().splitlines()[self.loc.line].decode("utf-8")
+		offending_code = offending_code.replace('\t', ' ' * TAB_WIDTH)
+
+		trimmed_code = "    " + offending_code.lstrip()
+
+		arrow = "    " + (' ' * (self.loc.column - len(offending_code) - len(trimmed_code))) + '^'
+
+		print(f"{' ' * (gutter_width - 2)}| ")
+		print(f" {1 + self.loc.line} | {trimmed_code}")
+		print(f"{' ' * (gutter_width - 2)}| {colourise(arrow, '1;31m')}")
 		sys.exit(1)
 
